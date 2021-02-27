1582206f36fccfa2c61c321283e651169769dfd6
Molorane Mothusi <molorane.mothusi@gmail.com>
2021-02-27 14:54:09 +0200

    Lambdas mothusi

---
+++ src/BlockLambdaDemo2/StringFunc.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BlockLambdaDemo2;
+
+/**
+ *
+ * @author madw
+ */
+public interface StringFunc {
+    String func(String n);
+}
---
+++ src/ConstructorRefDemo2/MyClass.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ConstructorRefDemo2;
+
+/**
+ *
+ * @author madw
+ */
+class MyClass<T> {
+
+    private T val;
+    // A constructor that takes an argument.
+
+    MyClass(T v) {
+        val = v;
+    }
+    // This is the default constructor.
+
+    MyClass() {
+        val = null;
+    }
+    // ...  
+    T getVal() { return val; };
+}
